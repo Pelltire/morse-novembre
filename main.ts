43235d97a2c75faf2a566d15842757cdc82f1f41
Pelltire <73190861+Pelltire@users.noreply.github.com>
2020-11-10 14:50:51 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,113 @@
+function E () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+    basic.pause(500)
+}
+function O () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+}
+function M () {
+    for (let index = 0; index < 2; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+}
+function B () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(50)
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+}
+function V () {
+    for (let index = 0; index < 3; index++) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+        basic.pause(50)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.clearScreen()
+    basic.pause(500)
+}
+function R () {
+	
+}
+basic.forever(function () {
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.clearScreen()
+    basic.pause(50)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(50)
+    basic.clearScreen()
+    basic.pause(500)
+})
